53a2913852adcf4cb5d79822b6c26cfe9310bd79
markasif <apasif243@gmail.com>
2025-01-10 22:11:09 +0530

    updated exception and logging

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,24 @@
+import sys
+from networksecurity.logging import logger
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))  # Adds the root directory to sys.path
+
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error Occur in Python  Script Name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name,self.lineno,str(self.error_message))
+
+if __name__ == "__main__":
+    try:
+        logger.logging("Entered in try block")
+        a=1/0
+        print("This will not be Printed",a)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 from datetime import datetime
 
 LOG_FILE =f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
@@ -13,5 +12,5 @@ LOG_FILE_PATH =os.path.join(log_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
 )
