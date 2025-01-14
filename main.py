553b006f76c0a7d7d83e1f69d37cea584bfd3b16
markasif <apasif243@gmail.com>
2025-01-14 18:04:51 +0530

    added data validation to project

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation,DataValidationConfig
 import sys
 
 if __name__ =="__main__":
@@ -13,5 +14,14 @@ if __name__ =="__main__":
          logging.info("Initate the data ingestion")
          dataingestionartifacts=data_ingestion.initiate_data_ingestion()
          print(dataingestionartifacts)
+         logging.info("Data initiation completed")
+         data_validation_config = DataValidationConfig(trainingpipelineconfig)
+         data_validation =DataValidation(dataingestionartifacts,data_validation_config)
+         logging.info("Initate the data validation")
+         data_validation_Artifact=data_validation.initiate_data_validation()
+         logging.info("data validation is completed")
+         print(data_validation_Artifact)
+
+    
     except Exception as e:
         raise NetworkSecurityException(e,sys)
